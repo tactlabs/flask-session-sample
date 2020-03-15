af912d3ee6b08fc8f096546efed5065b78fb2f9f
Raja CSP Raman <raja.csp@gmail.com>
2020-03-15 12:11:15 -0400

    Session, logout, decorator added

--- app.py
+++ app.py
@@ -21,12 +21,43 @@ Source:
 from flask import Flask
 import json
 from flask import jsonify
+from functools import wraps
 
 from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session
 
 app = Flask(__name__)
 
 app.secret_key = "LaEt{B07|MCCtC0.(OGw0B[-AUTs"
+
+SESSION_KEY = "user_session"
+
+def requires_session(f):
+  
+    @wraps(f)
+    def decorated(*args, **kwargs):
+
+        # print('inside deco')
+    
+        # check apikey in args
+        if SESSION_KEY not in session:
+
+            # print('session not available')
+
+            data = {
+                'apiresult' : 'Session Not Available',
+                'apimessage': 1011
+            }
+
+            return jsonify(data)
+
+        # print('session is available')
+
+        # verify user_session
+        user_session = session.get(SESSION_KEY)
+
+        return f(*args, **kwargs)
+
+    return decorated
 
 '''
     http://127.0.0.1:5000/add-visit/
@@ -70,8 +101,48 @@ def entry_point():
     http://localhost:5000/content
 '''
 @app.route('/content')
+@requires_session # Decorator has to be close to the method
 def user_content():
     return 'User Content'
+
+'''
+    http://localhost:5000/login
+
+    http://localhost:5000/login?email=raja@gmail.com&password=abcd
+    http://localhost:5000/login?email=one&password=two
+'''
+@app.route('/login')
+def login():
+
+    email = request.values.get('email')
+    password = request.values.get('password')
+
+    if(email == 'raja@gmail.com' and password == 'abcd'):
+
+        # set session
+        session[SESSION_KEY] = 'session_key_raja'
+
+        return "Success"
+
+    return 'Login Failed'
+
+'''
+    http://localhost:5000/get/session
+'''
+@app.route('/get/session')
+def get_session():
+
+    return session[SESSION_KEY]
+
+'''
+    http://localhost:5000/logout
+'''
+@app.route('/logout')
+def logout():
+
+    del session[SESSION_KEY]
+
+    return "Logged out"
 
 
 if __name__ == '__main__':
