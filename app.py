29dd060d8a751cc72abc929a8367a531e80bc80c
Raja CSP Raman <raja.csp@gmail.com>
2020-03-15 11:38:44 -0400

    Session code updatd

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ Created on
 Course work: 
 @author: raja
 Source:
+
+    https://overiq.com/flask-101/sessions-in-flask/
+
+    https://stackoverflow.com/questions/26080872/secret-key-not-set-in-flask-session-using-the-flask-session-extension
+
+    https://randomkeygen.com/
     
+    https://pythonhosted.org/Flask-Session/ - this is buggy and deprecated
 '''
 
 # Import necessary modules
@@ -15,7 +22,42 @@ from flask import Flask
 import json
 from flask import jsonify
 
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session
+
 app = Flask(__name__)
+
+app.secret_key = "LaEt{B07|MCCtC0.(OGw0B[-AUTs"
+
+'''
+    http://127.0.0.1:5000/add-visit/
+'''
+@app.route('/add-visit/')
+def visits():
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1  # reading and updating session data
+    else:
+        session['visits'] = 1 # setting session data
+    return "Total visits: {}".format(session.get('visits'))
+
+'''
+    http://127.0.0.1:5000/delete-visit/
+'''
+@app.route('/delete-visit/')
+def delete_visits():
+    session.pop('visits', None) # delete visits
+    return 'Visits deleted'
+
+'''
+    http://127.0.0.1:5000/visits-counter/
+'''
+@app.route('/visits-counter/')
+def count():
+    if 'visits' in session:
+        session['visits'] = session.get('visits')  # reading and updating session data
+    else:
+        session['visits'] = 0 # setting session data
+    return "Total visits: {}".format(session.get('visits'))
+
 
 '''
     http://localhost:5000/
