952c7ba1548e272d50d111560e7850ff3163be3e
TactLabs2 <tactlabs2@TactLabs1s-MacBook-Air.local>
2024-11-05 12:19:32 +0530

    using flask-session and without session

--- business.py
+++ business.py
@@ -1,13 +1,26 @@
 from flask import Flask, session, redirect, url_for, request, render_template
+from flask_session import Session
 from datetime import timedelta
 
-# Initializes the Flask application.
 app = Flask(__name__)
 
-# secret key to secure the session data
-app.secret_key = 'abscsecretkey' 
+# Configure session to use filesystem
 
-app.permanent_session_lifetime =  timedelta(seconds=60)
+# the session_type is used to store the user data in the file 
+app.config['SESSION_TYPE'] = 'filesystem'
+
+# secret key to secure the session data
+app.config['SECRET_KEY'] = 'abscsecretkey' 
+
+
+app.config['SESSION_PERMANENT'] = True 
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=10)
+
+
+
+
+# Initialize the session
+Session(app)
 
 @app.route('/')
 def home():
@@ -15,41 +28,30 @@ def home():
         return redirect(url_for('dashboard'))
     return redirect(url_for('login'))
 
-
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if  request.method =='POST':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
         if username == 'admin' and password == 'password':
-            session.permanent = True
-            session['user'] = username
+            session['user'] = username  # Store user in session
+            session.permanent = True  # Mark the session as permanent (this activates the 60-second timeout)
             return redirect(url_for('dashboard'))
         return "Invalid credentials, please try again."
     return render_template('login.html')
-
 
 @app.route('/dashboard')
 def dashboard():
     if 'user' in session:
         user = session['user']
-        return render_template('dashboard.html', user= user)
+        return render_template('dashboard.html', user=user)
     return redirect(url_for('login'))
-
 
 @app.route('/logout')
 def logout():
-    session.pop('user',None)
+    session.pop('user', None)  # Remove user from session
     return redirect(url_for('login'))
-
-
-# This function is executed before every request.
-@app.before_request
-def make_session_permanent():
-    session.permanent   = True
-    # session.modified    = True
-
 
 if __name__ == '__main__':
     app.run(debug=True)
